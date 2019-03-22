e39c6bb117cbe4af0e09dfe5c6a01d245c548399
許仙 <xuxiandaguanren@gmail.com>
2019-03-22 11:29:24 +0800

    添加属性

--- TTProgressHud/TTProgressHud/TTProgressHud/TTProgressHud.swift
+++ TTProgressHud/TTProgressHud/TTProgressHud/TTProgressHud.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import UIKit
+
+class TTProgressHud: NSObject {
+    /// 单利
+    @objc static let shared = TTProgressHud()
+    /// 获取windowView
+    private let tpWindow = UIApplication.shared.keyWindow
+    /// 屏幕宽高
+    private let TPScreenWidht = UIScreen.main.bounds.width
+    private let TPScreenHeight = UIScreen.main.bounds.height
+    /// 创建提示View
+    private var promptView = TTProgressView()
+    /// 网络请求加载动画
+    private var loagView = TTLoadingAnimation()
+    /// 是否正在显示加载动画
+    private var isShowing: Bool = false
+    /// 加载动画完成回调
+    var loadingAnimationBlock: (()->Void)?
+    /// 提示动画完成回调
+    var loadingMessageBlock: (()->Void)?
+    
+    
+}
