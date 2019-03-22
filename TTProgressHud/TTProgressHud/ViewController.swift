0948024f2606e46536d4e26db2a82dbba62cb2da
許仙 <xuxiandaguanren@gmail.com>
2019-03-22 10:45:29 +0800

    加载动画

--- TTProgressHud/TTProgressHud/ViewController.swift
+++ TTProgressHud/TTProgressHud/ViewController.swift
@@ -12,9 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupUI()
     }
 
-
+    @objc func clickBtn() -> Void {
+       
+        let loadAnimation = TTLoadingAnimation()
+        
+        loadAnimation.loadingAnimation(frame: self.view.bounds)
+//        loadAnimation.replicatorLayerColor = UIColor.blue
+        
+        self.view.addSubview(loadAnimation)
+    }
+    
+    
+    func setupUI() -> Void {
+        let btn = UIButton.init(frame: CGRect(x: 100, y: 100, width: 120, height: 60))
+        btn.setTitle("动画", for: .normal)
+        btn.backgroundColor = UIColor.red
+        btn.addTarget(self, action: #selector(clickBtn), for: .touchUpInside)
+        self.view.addSubview(btn)
+    }
 }
 
